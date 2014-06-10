1f9fe480bc6ce1b0826a1749b75879dfc6ec688c
Thiago Dutra Chiarato <thiago.chiarato@hotmail.com>
2014-06-10 00:49:14 -0300

    sort

--- Chaordic/app.js
+++ Chaordic/app.js
@@ -2,19 +2,21 @@ var ss      = require('simplestorm'),
     Spout   = require('./controller/spout'),
     Reader  = require('./controller/reader'),
     Reduce  = require('./controller/reduce'),
-    Map     = require('./controller/map');
+    Map     = require('./controller/map'),
+    Sort    = require('./controller/sort');
 
 var spout  = new Spout(),
     reader = new Reader(),
     reduce = new Reduce(),
-    map    = new Map();
+    map    = new Map(),
+    sort   = new Sort();
 
 var builder = ss.createTopologyBuilder();
 
-var start = Date.now();
 function Timer(){
+    this.start = Date.now();
     this.process = function(data, context) {
-        console.log('finished: ' + ((Date.now() - start)/1000).toFixed(1));
+        console.log('finished: ' + ((Date.now() - this.start)/1000).toFixed(1));
     }
 }
 
@@ -24,7 +26,8 @@ builder.setSpout("spout", spout);
 builder.setBolt("reader", reader).shuffleGrouping("spout");
 builder.setBolt("reduce", reduce).shuffleGrouping("reader");
 builder.setBolt("map", map).shuffleGrouping("reduce");
-builder.setBolt("timer", timer).shuffleGrouping("map");;
+builder.setBolt("sort", sort).shuffleGrouping("map");
+builder.setBolt("timer", timer).shuffleGrouping("sort");;
 
 var topology = builder.createTopology();
 topology.start();
